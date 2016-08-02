14eec2b892ab9f309ab7e64a1e238c297bea89d5
mary <grimesmea@gmail.com>
2016-08-02 14:32:21 -0400

    fix: Clean dist/ directory

--- dist/js/404.js
+++
@@ -1,7 +0,0 @@
-/**
- - 0.1.0
-
-Copyright (c) 2016 Cloud Giant Games
-License: 
-*/
-$(document).ready(function(){$("#background").mouseParallax({moveFactor:3}),$("#foreground").mouseParallax({moveFactor:10})});
--- dist/js/main.js
+++
@@ -1,6 +0,0 @@
-/**
- - 0.1.0
-
-Copyright (c) 2016 Cloud Giant Games
-License: 
-*/
